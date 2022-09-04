6fc52a12313e5603495e5ab569f51dd0ebbd4a77
Philipp Lenz <philipp.lenz@hotmail.com>
2022-09-05 00:47:12 +0200

    Update UNet3D.py

--- UNet3D.py
+++ UNet3D.py
@@ -1,13 +1,13 @@
-from operator import add
 import torch
 import torch.nn as nn
 
+kernel_xy_size = 3
 
 def double_conv(in_c, out_c):
     conv = nn.Sequential(
-        nn.Conv3d(in_c, out_c, kernel_size=(3, 5, 5), padding=1),
+        nn.Conv3d(in_c, out_c, kernel_size=(3, kernel_xy_size, kernel_xy_size), padding=1),
         nn.ReLU(inplace=True),
-        nn.Conv3d(out_c, out_c, kernel_size=(3, 5, 5), padding=1),
+        nn.Conv3d(out_c, out_c, kernel_size=(3, kernel_xy_size, kernel_xy_size), padding=1),
         nn.ReLU(inplace=True)
     )
     return conv
@@ -25,10 +25,10 @@ class UNet3D(nn.Module):
     
     def __init__(self, n_out):
         # TODO: bump those up to a multiple
-        n_chan_1 = 1
-        n_chan_2 = 2
-        n_chan_3 = 4
-        n_chan_4 = 8
+        n_chan_1 = 8
+        n_chan_2 = 16
+        n_chan_3 = 32
+        n_chan_4 = 64
         
         super(UNet3D, self).__init__()
 
@@ -38,17 +38,17 @@ class UNet3D(nn.Module):
         self.down_conv_3 = double_conv(n_chan_2, n_chan_3)
         self.down_conv_4 = double_conv(n_chan_3, n_chan_4)
         
-        self.up_trans_1 = nn.ConvTranspose3d(n_chan_4, n_chan_3, kernel_size=(2, 5, 5), stride=2)
+        self.up_trans_1 = nn.ConvTranspose3d(n_chan_4, n_chan_3, kernel_size=(2, kernel_xy_size, kernel_xy_size), stride=2)
         self.up_conv_1 = double_conv(n_chan_4, n_chan_3)
-        self.up_trans_2 = nn.ConvTranspose3d(n_chan_3, n_chan_2, kernel_size=(3, 5, 5), stride=2)
+        self.up_trans_2 = nn.ConvTranspose3d(n_chan_3, n_chan_2, kernel_size=(3, kernel_xy_size, kernel_xy_size), stride=2)
         self.up_conv_2 = double_conv(n_chan_3, n_chan_2)
-        self.up_trans_3 = nn.ConvTranspose3d(n_chan_2, n_chan_1, kernel_size=(2, 5, 5), stride=2)
+        self.up_trans_3 = nn.ConvTranspose3d(n_chan_2, n_chan_1, kernel_size=(2, kernel_xy_size, kernel_xy_size), stride=2)
         self.up_conv_3 = double_conv(n_chan_2, n_chan_1)
 
         # TODO: do not use kernel size of 10 but kernel size=1
-        self.out = nn.Conv3d(n_chan_1, 1, kernel_size=(10, 1, 1))
+        self.out = nn.Conv3d(n_chan_1, 2, kernel_size=(10, 1, 1))
 
-        self.fc = nn.Linear(96889, n_out)
+        self.fc = nn.Linear(2 * 325 * 450, n_out)
 
     
     def forward(self, x):
@@ -61,21 +61,21 @@ class UNet3D(nn.Module):
         x5 = self.down_conv_3(x4) #
         x6 = self.max_pool(x5)        
         x7 = self.down_conv_4(x6) #
-        ## x7.shape: 128, 1, 33, 48
-
+        
         x = self.up_trans_1(x7)
-        y = crop_tensor(x5, x)
-        x = self.up_conv_1(torch.cat([x, y], 1))
+        x = x[:, :, :, :, :-1]
+        x = self.up_conv_1(torch.cat([x, x5], 1))
 
         x = self.up_trans_2(x)
-        y = crop_tensor(x3, x)
-        x = self.up_conv_2(torch.cat([x, y], 1))
+        x = x[:, :, :, :-1, :]
+        x = self.up_conv_2(torch.cat([x, x3], 1))
 
         x = self.up_trans_3(x)
-        y = crop_tensor(x1, x)
-        x = self.up_conv_3(torch.cat([x, y], 1))
-
-        x = self.out(x)
+        x = x[:, :, :, :, :-1]
+        x = self.up_conv_3(torch.cat([x, x1], 1))
+        
+        # x = self.out(x)
         x = torch.flatten(x, 1) # flatten all dimensions except the batch dimension
+
         return self.fc(x)
         
