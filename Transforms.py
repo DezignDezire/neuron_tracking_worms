b9d9d1070c2efeb76978c3e55e2a3b910eba771d
Philipp Lenz <philipp.lenz@hotmail.com>
2022-10-26 17:16:15 +0200

    Update transforms

--- Transforms.py
+++ Transforms.py
@@ -20,11 +20,9 @@ class Transforms:
         self.flip_prob = flip_prob
         self.applied_mode = None
 
-        ##
-        # self.
-
     
     def __call__(self, image = None, positions = None):
+
         if random.random() < self.rotation_prob:
             angle = random.choice(list(range(self.max_rot_angle)))
             image =  self.rotate_image(image, angle)
@@ -41,9 +39,6 @@ class Transforms:
 
 
 
-
-
-
     def rotate_image(self, image, angle):
         if image == None:
             return None
@@ -51,7 +46,7 @@ class Transforms:
         return TF.rotate(image, angle)
 
     def rotate_positions(self, positions, angle):
-        if positions == None:
+        if len(positions) == 0:
             return None
 
         radians = torch.tensor(angle * math.pi / 180)
@@ -73,38 +68,8 @@ class Transforms:
         return torch.flip(image, dims)
 
     def flip_positions(self, positions, mode):
-        if positions == None:
+        if len(positions) == 0:
             return None
-        
-        positions = positions * mode
+        positions = torch.tensor(positions) * mode
         return positions
 
-
-    # def random_crop(image, positions):
-    # positions = torch.tensor(positions)
-    # img_shape = image.shape
-    # crop_sizes_xy = [300, 300]
-    # center_of_mass_positions = torch.mean(positions, dim=0)
-    # print(center_of_mass_positions)
-    # positions = positions - center_of_mass_positions
-    # center_of_mass_positions = torch.unsqueeze(torch.tensor(center_of_mass_positions), 0)
-    # center_of_mass = retransform_points(center_of_mass_positions)
-
-    # print('ceeeenter', center_of_mass)
-    # image = TF.crop(image, int(center_of_mass[0,1] - crop_sizes_xy[1]/2), int(center_of_mass[0,2] - crop_sizes_xy[0]/2), crop_sizes_xy[0], crop_sizes_xy[1])
-    # # TT.Pad starts at left side
-    # print(image.shape)
-    # assert(image.shape[2] == crop_sizes_xy[1])
-    # assert(image.shape[1] == crop_sizes_xy[0])
-    # padd_left_and_right = int((img_shape[2] - image.shape[2]) / 2)
-    # padd_top_and_bottom = int((img_shape[1] - image.shape[1]) / 2)
-    # print(padd_left_and_right, padd_top_and_bottom)
-    # image = TF.pad(image, (padd_left_and_right, padd_top_and_bottom))
-    # print(image.shape)
-    # return image, positions
-
-
-
-## translations - shifting / moving
-
-## crops
